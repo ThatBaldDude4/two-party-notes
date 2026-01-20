9d7ec63237d99fb9d56ff5790de2e108065b75a1
ThatBaldDude4 <taylorv0004@gmail.com>
2026-01-19 21:39:12 -0600

    Refactored code to have state object be center of truth, rewriting essential functions such as renderList, and render

--- script.js
+++ script.js
@@ -1,92 +1,52 @@
-const appContainer = document.getElementById("app-container");
-const appDisplay = document.getElementById("app-display");
-const editButtons = document.querySelectorAll(".edit-button");
-const addNoteBtn = document.getElementById("add-note");
+const fullAppContainer = document.getElementById("app-container");
+const appDisplayContainer = document.getElementById("app-display");
 
-const allNotesArr = [];
-
-class NoteCard {
-  constructor(title, note, users) {
-    this.title = title;
-    this.note = note;
-    this.users = users;
-    this.createdAt = Date.now()
-  };
-  
-  editNote(type, newData) {
-    if (!this[type] || type === "id"){return}
-    this[type] = newData;
-  }
+class Note {
+    constructor(title, noteDes) {
+        this.title = title;
+        this.noteDescription = noteDes;
+        this.createdAt = Date.now();
+    }
 }
 
-function fillNote(noteObject) {
-  let {title, note} = noteObject
-  let createdAt = noteObject.createdAt;
-  let results = `
-  <div class="note-card">
-    <h2>${title}</h2>
-    <p>${note}</p>
-    <p>${createdAt}<p>
-    <button class="edit-button">Edit Note</button>
-   </div>
-  `
-  return results
+const state = {
+    view: "list",
+    notes: [],
+    activeNoteId: null,
 }
 
-function addNote(title, note, users) {
-  let noteObject = new NoteCard(title, note, users);
-  fillNote(noteObject);
-  allNotesArr.push(noteObject);
+function render(state) {
+    if (state.view === "list") {
+        renderList(state.notes);
+    }
 }
 
-function displayNoteForm() {
-    console.log("Inside displayNoteForm()")
-    const form = 
-    `
-    <label for="title">Title:</label>
-    <input id="title" name="title">
+function renderList(notes) {
+    let convertedNotes = ``;
 
-    <label for="description">Note:</label>
-    <input id="description" name="description">
+    notes.forEach(({title, noteDescription, createdAt}) => {
+        convertedNotes += 
+        `
+        <div class="notes">
+            <h2 class="note-title">${title}</h2>
+            <p class="note-description">${noteDescription}</p>
+            <div class="time-created">${createdAt}</div>
+        </div>
+        `
+    });
 
-    <label for="users">Users:</label>
-    <input id="users" name="users">
-
-    <button type="submit" id="submit-note-btn">Submit Note</button>
-    `;
-
-    appDisplay.innerHTML = form;
+    appDisplayContainer.innerHTML = convertedNotes;
 }
 
-function emptyNotes() {
-  appDisplay.innerHTML = "";
-}
 
-function renderNotes(dataArr) {
-  let parsedDataStr = ``;
-  dataArr.forEach((noteData) => {
-    let parsedData = fillNote(noteData);
-    console.log(parsedData)
-    parsedDataStr += parsedData;
-  })
-  appDisplay.innerHTML = parsedDataStr;
-}
 
-addNoteBtn.addEventListener("click", () => {
-    console.log("event fired")
-    emptyNotes();
-    displayNoteForm();
-})
 
-let note5 = new NoteCard("this is title", "testing this", [1])
-let note2 = new NoteCard("this is title", "testing this", [1])
-let note3 = new NoteCard("this is title", "testing this", [1])
-let note4 = new NoteCard("this is title", "testing this", [1])
-addNote("Testing Note Obj", "This is testing the function out", [1, 4, 6])
-allNotesArr.push(note5)
-allNotesArr.push(note2)
-allNotesArr.push(note3)
-allNotesArr.push(note4)
-console.log(allNotesArr);
+let note1 = new Note("Title", "This is description");
+let note2 = new Note("Title", "This is description");
+let note3 = new Note("Title", "This is description");
 
-renderNotes(allNotesArr);
+state.notes.push(note1);
+state.notes.push(note2);
+state.notes.push(note3);
+
+renderList(state.notes); 
